267260c8685d44f3f2569e38e4330f1c8638d955
zahangir <alamzahangir218@gmail.com>
2025-05-12 23:53:03 +0600

    some code witre

--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 
+
 // show categories
 const loadCategories = () => {
     // console.log("create category")
@@ -7,6 +8,61 @@ const loadCategories = () => {
     .then ((data) =>displayCategories(data.categories))
     .catch ((error) => console.log(error))
 }
+
+// const demo = {
+    
+//     "petId": 11,
+//     "breed": "French Bulldog",
+//     "category": "Dog",
+//     "date_of_birth": "2023-07-20",
+//     "price": 2500,
+//     "image": "https://i.ibb.co.com/47Sxf3X/pet-11.jpg",
+//     "gender": "Male",
+//     "pet_details": "This adorable male French Bulldog, born on July 20, 2023, is known for his playful and affectionate nature. Fully vaccinated and priced at $2500, he makes a perfect companion for apartment living.",
+//     "vaccinated_status": "Fully",
+//     "pet_name": "Ollie"
+
+// }
+
+const loadPets = () => {
+fetch("https://openapi.programming-hero.com/api/peddy/pets")
+.then((res) =>res.json())
+.then((data) =>displayPets(data.pets))
+.catch((error) =>console.log(error))
+}
+const displayPets = (pets) => {
+// console.log(pets)
+const petContainaer = document.getElementById("pets");
+pets.forEach(pet => {
+console.log(pet)
+const card = document.createElement("div");
+card.classList="card card-compact";
+card.innerHTML=`
+ <figure class="h-[200px]">
+    <img class="h-full w-full object-cover"
+      src=${pet.image}
+      alt="Shoes" />
+  </figure>
+  <div class="px-0 py-2 ">
+    <div>
+    <h1 class="font-bold">${pet.pet_name}</h1>
+    <P>Breed :${pet.breed}</p>
+    <P>Birth :${pet.date_of_birth}</p>
+    <P>Gender :${pet.gender}</p>
+    <P>Price :${pet.price}</p>
+    </div>
+    <div class="">
+    <button ${onclick(console.log("click"))} id="pet-img" class="btn btn-primary">Adopt</button>
+    <button class="btn btn-primary">Details</button>
+    </div>
+  </div>
+`;
+  petContainaer.append(card);
+})
+}
+
+
+
 const displayCategories = (categories) =>{
     const categoriesContainer = document.getElementById('categories');
 // console.log(data)
@@ -24,6 +80,7 @@ const displayCategories = (categories) =>{
 
 
 loadCategories();
+loadPets();
 
 //  
 
